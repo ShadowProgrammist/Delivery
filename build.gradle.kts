c6bea5b44bf3b696a84013adc946da1212863746
ShadowProgrammist <serzh.frolov.2004@inbox.ru>
2025-06-13 21:34:11 +0300

    new package create

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -23,7 +23,7 @@ publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
-            artifactId = "Delivery"
+            artifactId = "delivery"
         }
     }
     repositories {
